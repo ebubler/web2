2144c0509d81586ad2ee5ee711131eaa3793f8a1
Ilya <make.reigw@gmail.com>
2025-02-28 19:15:54 +0500

    Готовимся к миссии + Двойная защита

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    id_astr = StringField('id астронавта', validators=[DataRequired()])
+    password_astr = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_cap = StringField('id капитана', validators=[DataRequired()])
+    password_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Доступ')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 data = {
     "title": "Автоматический ответ",
@@ -26,10 +38,9 @@ professions = [
     "гляциолог"
 ]
 
-@app.route('/')
-@app.route('/index')
-def index():
-    title = "Главная страница"
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
     return render_template('base.html', title=title)
 
 @app.route('/training/<prof>')
@@ -60,6 +71,17 @@ def list_prof(list_type):
 @app.route('/auto_answer')
 def auto_answer():
     return render_template('auto_answer.html', **data)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Успешный вход')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
